419ea068192be8afd92e51c8dc43f7c94350504f
Mazor Leave <mazor9920@gmail.comn>
2021-12-08 23:47:44 +0200

    fix horizontal scrolling for submission

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// for the Console
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
